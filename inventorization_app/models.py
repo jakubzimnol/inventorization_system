978235853f10abf2dad9cd335a9bbf5e70859990
jakubzimnol <jakubdodek@poczta.onet.pl>
2018-09-05 04:03:16 -0700

    refactorized code in model and added router to urls

--- inventorization_app/models.py
+++ inventorization_app/models.py
@@ -6,8 +6,6 @@ class Products(models.Model):
     name = models.CharField(max_length=120)
     category = models.CharField(max_length=120)
     owner = models.ForeignKey('auth.User', related_name='products', blank=True, null=True, on_delete=models.SET_NULL)
-    #owner = models.ForeignKey(User, null=True, on_delete= models.SET_NULL, related_name='my_products')
-    #deny_token = models.CharField(max_length=10)
-    allow_token = models.CharField(max_length=10, default='')
+    allow_token = models.CharField(max_length=20, default='')
     def get_api_url(self):
         return api_reverse("products:detail", kwargs={'pk':self.id})
